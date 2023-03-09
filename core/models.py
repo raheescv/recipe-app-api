a5880ac9e9810b2a7205daeeac56dbf01208c3f2
raheescv <raheescv1992@gmail.com>
2023-03-09 15:21:26 +0530

    added more test cases

--- core/models.py
+++ core/models.py
@@ -1,3 +1,45 @@
-from django.db import models  # noqa
+"""
+Database models.
+"""
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    """Manager for users."""
+    def create_user(self, email, password=None, **extra_fields):
+        """Create ,save and return a new user."""
+        if not email:
+            raise ValueError("User Must have an email address")
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using = self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create ,save and return a new superuser."""
+        if not email:
+            raise ValueError("SuperUser Must have an email address")
+        user = self.model(email=self.normalize_email(email))
+        user.set_password(password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+    email = models.EmailField(max_length=254, unique=True)
+    name = models.CharField(max_length=254, unique=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
